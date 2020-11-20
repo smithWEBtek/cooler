c3d3db0deea881e2c3f2b9dc6d3400c4f625d7ad
smithwebtek <brad@smithwebtek.com>
2020-11-19 19:55:19 -0500

    Prep deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "cooler-communities"
-set :repo_url, "git@github.com:MAPC/cooler-communities.git"
+set :application, "cooler"
+set :repo_url, "git@github.com:smithWEBtek/cooler-communities2.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, "main"
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/cooler"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -18,11 +19,11 @@ set :repo_url, "git@github.com:MAPC/cooler-communities.git"
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/master.key"
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
@@ -34,7 +35,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,11 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "live.mapc.org", user: "cooler-communities", roles: %w{app db web}
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
-
-
+# server "cooler.smithwebtek.com", user: "deploy", roles: %w{app db web}
+server "67.205.165.187", user: "deploy", roles: %w{app db web}
 
 # role-based syntax
 # ==================
