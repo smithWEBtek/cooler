19a3937dc585c6d72d8ae85d16bd0933d3fbf63d
smithwebtek <brad@smithwebtek.com>
2020-11-19 20:32:16 -0500

    Set Cap lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.14.0"
 
 set :application, "cooler"
 set :repo_url, "git@github.com:smithWEBtek/cooler-communities2.git"
