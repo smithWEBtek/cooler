3a19ead759cd6ed272b34547ed44e09d46c3069a
brad@smithwebtek.com <brad@smithwebtek.com>
2024-10-13 10:42:43 -0400

    Update deploy.rb point to repo, regen credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.0"
 
 set :application, "cooler"
-set :repo_url, "git@github.com:smithWEBtek/cooler-communities2.git"
+set :repo_url, "git@github.com:smithWEBtek/cooler.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -23,7 +23,7 @@ set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/master.key"
-append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/database.yml", "config/master.key", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
